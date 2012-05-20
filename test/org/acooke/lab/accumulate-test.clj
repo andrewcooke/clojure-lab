09ffe88bc5b6199fa0a90bf8bc7ef4ea903d02a8
andrew cooke <andrew@acooke.org>
2012-05-20 09:28:17 -0400

    make seq type explicit in tests

--- test/org/acooke/lab/accumulate-test.clj
+++ test/org/acooke/lab/accumulate-test.clj
@@ -4,18 +4,18 @@
 
 
 (deftest test-my-range-1
-  (is (= [0 1 2] (my-range-0 0 3))))
+  (is (= '(0 1 2) (my-range-0 0 3))))
 
 (deftest test-my-range-1
-  (is (= [2 1 0] (my-range-1 0 3))))
+  (is (= '(2 1 0) (my-range-1 0 3))))
 
 (deftest test-my-range-2
-  (is (= [0 1 2] (my-range-2 0 3))))
+  (is (= '(0 1 2) (my-range-2 0 3))))
 
 (deftest test-my-range-3
   (is (= [0 1 2] (my-range-3 0 3))))
 
 (deftest test-my-range-4
-  (is (= [2 1 0] (my-range-4 0 3))))
+  (is (= '(2 1 0) (my-range-4 0 3))))
 
 (run-tests)
