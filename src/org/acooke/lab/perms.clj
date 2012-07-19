479edbbc8da23c6a6add7d273e8335eb691a41ec
andrew cooke <anderw@acooke.org>
2012-07-18 23:42:33 -0400

    perms

--- src/org/acooke/lab/perms.clj
+++ src/org/acooke/lab/perms.clj
@@ -1,8 +1,10 @@
 (ns org.acooke.lab.perms)
 
+; http://stackoverflow.com/questions/11530574/clojure-for-comprehension-example
+
 (defn stream [seed]
   (defn helper
-    ([slow] (let [slow (seq slow)] (helper (first slow) seed (rest slow))))
+    ([slow] (helper (first slow) seed (rest slow)))
     ([c fast slow]
       (let [fast (seq fast)]
         (if fast
